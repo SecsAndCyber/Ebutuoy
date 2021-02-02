dd5a09001c5bd685f238705ca1ac6cca3a34674c
Matthew Molyett <matthewmolyett@gmail.com>
2021-02-02 11:09:26 -0500

    license fix

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright (c) 2021 Matthew Molyett. All rights reserved.
 
 /**
  * @filedescription Initializes the extension's background page.
