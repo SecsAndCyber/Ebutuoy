7dc67b661893bbeb92c831e74e0599ea93709d60
Matthew Molyett <matthewmolyett@gmail.com>
2024-01-28 12:12:09 -0500

    Update

--- ebutuoy.js
+++ ebutuoy.js
@@ -29,23 +29,10 @@ EbutuoyObject.prototype = {
    * @private
    */
   onBeforeSendHeaders_: function(data) {
-	  if(! data.url.match(/^.{10,25}youtube/)){ return; }
-	  
-	  var skip_types = ['ping','image','script', 'stylesheet', 'other'];
-	  var skip_apis_re = /youtube.*(comment_service_ajax|error_204|get_midroll_info|v1\/next|log_event|\/player|notification\/get_unseen_count|api\/stats)/;
-	  var allow_requesters_re = /hcpss.instructure.com|molyett.com/;	  
-	  if(data.initiator && data.initiator.match(allow_requesters_re)) { return }
-	  if (data.url.match(/googlevideo/) && (data.initiator && data.initiator.match(/youtube/))) { return }
-	  if(skip_types.indexOf(data.type) >= 0){ return }	  
-	  if(data.url.match(skip_apis_re) && data.type.match(/xmlhttprequest/)){ return }
-	  	  
-	  if(data.url.match(/youtube.*watch/) && data.type.match(/xmlhttprequest/)){ return {cancel:true}; }
-	  if(data.url.match(/youtube.*embed/) && data.type.match(/sub_frame/)){ return {cancel:true}; }
-	  
-	  if(data.type.match(/main_frame/)) {	  
-		  if (data.url.match(/youtube/)) {
-			return {cancel:true};
-		  }
+	  if(! data.url.match(/^.{10,25}youtube/)){ 
+		return; 
 	  }
+	  if(data.initiator && data.initiator.match(/youtube/)) { return {cancel:true}; }
+	  return {cancel:true};
   }
 };
